13b9b605891b8a67f51c95a88a84fea16d38bbc3
fcmd <fcmd@animalprint.ca>
2021-06-27 00:50:34 -0700

    convert App component to class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import './App.css'
-import WorldMap from "./components/WorldMap";
-import Logo from "./components/Logo";
-import SetSail from "./components/SetSail";
+import WorldMap from "./components/WorldMap"
+import Logo from "./components/Logo"
+import SetSail from "./components/SetSail"
+import Page from "./Page";
 
-function App() {
-  return (
-    <div className="App">
-        <WorldMap>
-            <Logo/>
-            <SetSail/>
-        </WorldMap>
-    </div>
-  )
+interface AppState {
+  page: Page
+}
+
+class App extends React.Component<any, AppState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      page: Page.Title
+    }
+  }
+
+  setPage = (page: Page) => {
+    this.setState({ page: page })
+  }
+
+  render = () => {
+    switch(this.state.page) {
+      case Page.Title:
+        return (
+          <div className="App">
+            <WorldMap>
+              <Logo/>
+              <SetSail appStateSetter={this.setPage}/>
+            </WorldMap>
+          </div>
+        )
+      case Page.Input:
+        /* TODO: write the input page return statement */
+        break;
+      case Page.Results:
+        /* TODO: write the results page return statement */
+        break;
+    }
+  }
 }
 
 export default App
